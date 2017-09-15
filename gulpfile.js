0bc0cd77fb3a6c0614f9f7aee741c91e998b0d24
Anton Neverov <webpuritycg@gmail.com>
2017-09-15 12:39:33 +0900

    Gulp tasks add 2

--- gulpfile.js
+++ gulpfile.js
@@ -2,32 +2,32 @@ const gulp = require('gulp');
 const browserSync = require('browser-sync');
 const pug = require('gulp-pug');
 
+/*---------Pug compile---------*/
+
+gulp.task('templates:compile', function buildHTML() {
+   return gulp.src('source/template/*.pug')
+       .pipe(pug({
+           pretty: true
+       }))
+       .pipe(gulp.dest('build'));
+});
+
+/*---------Sass compile---------*/
+
+
+
 /*---------BrowserSync---------*/
 
 gulp.task('server', function () {
     browserSync.init({
-       server: {
-           port: 9000,
-           baseDir: "build"
-       }
+        server: {
+            port: 9000,
+            baseDir: "build"
+        }
     });
 
     gulp.watch('build/**/*').on('change', browserSync.reload);
 });
-
-/*---------Pug compile---------*/
-
-gulp.task('templates:compile', function () {
-   return gulp.src('views/*.pug')
-       .pipe(pug({
-
-       }))
-});
-
-
-
-
-
 
 
 
